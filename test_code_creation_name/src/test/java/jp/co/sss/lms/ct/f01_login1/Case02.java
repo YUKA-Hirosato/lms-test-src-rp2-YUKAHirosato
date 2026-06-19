f9c31f57d6e359d78c746f9422d33338f03c879a
Ibs-user <Ibs-user@PC-201809-06>
2026-06-19 11:43:44 +0900

    add:Case02.java　廣里　ログイン画面への遷移テストコード、DBに登録されていないユーザー（ログインId:Student99、password：Student99)を入力し、エラー文を比較するテストを作成した。 期待値：*&nbsp;ログインに失敗しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +24,9 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
 public class Case02 {
+
+	@LocalServerPort
+	private int port;
 
 	/** 前処理 */
 	@BeforeAll
@@ -36,6 +45,20 @@ public class Case02 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		WebDriverUtils webDriver = new WebDriverUtils();
+
+		// 指定のURLの画面を開く
+		//再利用性
+		//WebDriverUtils.goTo("http://localhost:" + port + "/lms");
+		webDriver.goTo("http://localhost:8080/lms/");
+
+		//Titleの取得とアサーション
+		assertEquals("ログイン | LMS", WebDriverUtils.webDriver.getTitle());
+
+		// 開いたページのキャプチャを取得する
+		WebDriverUtils.getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
@@ -43,6 +66,18 @@ public class Case02 {
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		//DBに登録されていないユーザーを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("Student999");
+		webDriver.findElement(By.id("password")).sendKeys("Student999");
+		//送信ボタン押して
+
+		WebElement classElement = webDriver.findElement(By.cssSelector(".help-inline.error"));
+		assertEquals("*&nbsp;ログインに失敗しました。", classElement.getText());
+
+		// 開いたページのキャプチャを取得する
+		WebDriverUtils.getEvidence(new Object() {
+		});
+
 	}
 
 }
