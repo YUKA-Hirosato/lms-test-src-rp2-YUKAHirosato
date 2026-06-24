afa9cad57460b2271658f554426caede382e7fad
Ibs-user <Ibs-user@PC-201809-06>
2026-06-24 17:54:04 +0900

    add:Case10.java 廣里　テスト03まで完了　テスト04「OK」を押下まで終了

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -18,6 +23,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース10 受講生 勤怠登録 正常系")
 public class Case10 {
+	WebDriverUtils utils = new WebDriverUtils();
 
 	/** 前処理 */
 	@BeforeAll
@@ -36,6 +42,10 @@ public class Case10 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		WebDriverUtils.goTo("http://localhost:8080/lms/");
+
+		assertEquals("ログイン | LMS", WebDriverUtils.webDriver.getTitle());
+
 	}
 
 	@Test
@@ -43,6 +53,14 @@ public class Case10 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.id("password")).sendKeys("StlmsAA01");
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		utils.visibilityTimeout(By.cssSelector(".navbar-brand"), 10);
+
+		assertEquals("コース詳細 | LMS", WebDriverUtils.webDriver.getTitle());
+
 	}
 
 	@Test
@@ -50,6 +68,11 @@ public class Case10 {
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		webDriver.findElement(By.linkText("勤怠")).click();
+		webDriver.switchTo().alert().accept();
+
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
 	}
 
 	@Test
@@ -57,6 +80,15 @@ public class Case10 {
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
 		// TODO ここに追加
+		//「出勤」ボタンを押下
+		webDriver.findElement(By.cssSelector("form input[value='出勤'")).click();
+		//「OK」を押下
+		webDriver.switchTo().alert().accept();
+		//表示確認
+		WebElement attendant = webDriver
+				.findElement(By.xpath("//th[contains(text(),'開始時間')]/following-sibling::td[@class='w80']"));
+		assertTrue(attendant.isDisplayed());
+
 	}
 
 	@Test
